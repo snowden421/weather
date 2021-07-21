98045bda0465a05e265de05020cc2ac4325a4edf
Snow <84701509+snowden421@users.noreply.github.com>
2021-07-20 17:57:59 -0700

    added api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-var searchBar = docu
+var apiKey = "9f952a484bb0642f0030c770704f46cd";
+var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=pasadena&appid=" + apiKey;
+var apiUrlOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=29.6911&lon=-95.2091&appid=" + apiKey;
+
+var getApiCurrent = function() {
+    fetch(apiUrlCurrent)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    });
+};
+
+var getApiOneCall = function() {
+    fetch(apiUrlOneCall)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    });
+};
+
+getApiCurrent();
+getApiOneCall();
