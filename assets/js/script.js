a611a03abc8fa1d6a6fc1d2db88cc3a38ec16096
Snow <84701509+snowden421@users.noreply.github.com>
2021-10-04 16:42:17 -0700

    added past city function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,11 @@
-var apiKey = "9f952a484bb0642f0030c770704f46cd";
-var apiUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=pasadena&appid=" + apiKey;
-var apiUrlOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=29.6911&lon=-95.2091&appid=" + apiKey;
-
-var getApiCurrent = function() {
-    fetch(apiUrlCurrent)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
-};
-
-var getApiOneCall = function() {
-    fetch(apiUrlOneCall)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
-};
-
-getApiCurrent();
-getApiOneCall();
+// create a function to store city search terms using localStorage
+function pastCity(city) {
+    localStorage.setItem("city-name", city);
+    
+    var cityStorage = document.createElement ("button");
+    cityStorage.setAttribute("class", "pastCity form-control");
+    cityStorage.setAttribute("id",city);
+    var pastCityResults = document.querySelector("#search");
+    pastCityResults.appendChild(cityStorage);
+    cityStorage.innerHTML=city;
+  };
